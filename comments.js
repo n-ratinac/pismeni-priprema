6e10b5bd02ddb30391d55241e759d335db229a5f
ratinac-nikola <ratinac.nikola@gmail.com>
2023-12-19 10:48:31 +0100

    primer podataka za sortiranje

---
+++ comments.js
@@ -0,0 +1,42 @@
+comments = [
+    {
+        text: "Uberrime comparo acsi sint.",
+        upvotes: -18,
+    },
+    {
+        text: "Carpo addo cum cilicium.",
+        upvotes: 9,
+    },
+    {
+        text: "Vulgivagus cauda cruciamentum quis studio traho audentia accendo.",
+        upvotes: 21,
+    },
+    {
+        text: "Demoror tendo sunt viridis sollers deduco deripio bos.",
+        upvotes: 38,
+    },
+    {
+        text: "Tredecim curis clamo quas cupiditas doloremque cursus.",
+        upvotes: -1,
+    },
+    {
+        text: "Delectus vitium sopor.",
+        upvotes: 37,
+    },
+    {
+        text: "Una varietas reiciendis corporis tutis victoria suasoria voluntarius tactus.",
+        upvotes: 37,
+    },
+    {
+        text: "Suffragium tabula eius defessus adulatio decens utor.",
+        upvotes: 6,
+    },
+    {
+        text: "Universe dapifer ago crepusculum alias conscendo.",
+        upvotes: 14,
+    },
+    {
+        text: "Concido cibo censura comburo id vomer totidem.",
+        upvotes: 3,
+    },
+];
